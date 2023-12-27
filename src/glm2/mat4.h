ed6d0b7881206d2492c9d23af5392e9ecaad9ec8
Szobek Ádám Márk <szobek.adam@gmail.com>
2023-12-27 18:32:42 +0100

    lin algebruh nummero uno

---
+++ src/glm2/mat4.h
@@ -0,0 +1,33 @@
+#ifndef MAT4_H
+#define MAT4_H
+
+#include "vec3.h"
+
+typedef struct {
+	float data[16];
+}mat4;
+
+mat4 mat4_create(float szam);
+mat4 mat4_create2(float* data);
+
+
+mat4 mat4_sum(mat4 egy, mat4 katto);
+mat4 mat4_multiply(mat4 egy, mat4 katto);
+
+mat4 mat4_transpose(mat4 mat);
+mat4 mat4_inverse(mat4 mat);
+
+
+mat4 mat4_scale(mat4 mat, vec3 scale);
+
+mat4 mat4_rotate(mat4 mat, vec3 axis, float angle);
+
+mat4 mat4_translate(mat4 mat, vec3 pos);
+
+
+mat4 mat4_lookAt(vec3 pos, vec3 direction, vec3 up);
+
+mat4 mat4_perspective(float fov, float aspectXY, float near, float far);
+mat4 mat4_ortho(float left, float right, float bottom, float top, float near, float far);
+
+#endif
---
+++ src/glm2/vec3.h
@@ -0,0 +1,10 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+} vec3;
+
+#endif
