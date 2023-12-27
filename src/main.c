a22574962ea3f7e712b3eb1e3aefc2db34c8c3dc
mandliors <mandliors@gmail.com>
2023-12-27 18:10:55 +0100

    linux

--- src/main.c
+++ src/main.c
@@ -9,15 +9,16 @@ int main() {
     
     GLFWwindow* window = initWindow("amogus", 600, 600);
 
+    glfwTerminate();
+
     return 69;
 }
 
 GLFWwindow* initWindow(const char* name, int width, int height)
 {
-
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
